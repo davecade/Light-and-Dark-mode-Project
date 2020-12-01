36f629bbfbf63b643470ca422d42f12120980176
Dave <dave.cadelina@outlook.com>
2020-12-02 00:15:16 +1100

    added Undraw images and dynamically render dark images with JS

--- js/app.js
+++ js/app.js
@@ -1,19 +1,71 @@
-const toggleTheme = document.querySelector('input')
-const currentMode = document.querySelector('.mode')
-const theDOM = document.querySelector('html')
-const currentIcon = document.querySelector('.fas')
 
-toggleTheme.addEventListener('change', event => {
-    if(event.target.checked) {
-        currentMode.innerHTML = "Dark Mode";
-        theDOM.setAttribute("data-theme", "dark")
-        currentIcon.classList.replace('fa-sun', 'fa-moon')
-        
-    } else {
-        currentMode.innerHTML = "Light Mode";
-        theDOM.removeAttribute("data-theme")
-        currentIcon.classList.replace('fa-moon', 'fa-sun')
-        
+const App = (() => {
+
+    // -- Cache the DOM
+    const toggleTheme = document.querySelector('input')
+    const currentMode = document.querySelector('.mode')
+    const theDOM = document.querySelector('html')
+    const currentIcon = document.querySelector('.fas')
+    const $images = document.querySelector('.images')
+
+    // -- State
+    const state = {
+        darkMode: false
     }
-    console.log("HELLO")
-})
+
+    const listeners = () => {
+        toggleTheme.addEventListener('change', event => {
+            if(event.target.checked) {
+                currentMode.innerHTML = "Dark Mode";
+                theDOM.setAttribute("data-theme", "dark")
+                currentIcon.classList.replace('fa-sun', 'fa-moon')
+                state.darkMode = true
+                
+            } else {
+                currentMode.innerHTML = "Light Mode";
+                theDOM.removeAttribute("data-theme")
+                currentIcon.classList.replace('fa-moon', 'fa-sun')
+                state.darkMode = false
+                
+            }
+            render();
+        })
+    }
+
+    const render = () => {
+        let markup = ''
+
+        markup = `
+            <div class="img-container">
+                <h2>Fundamentals</h2>
+                <img src="images/undraw_static_website_${state.darkMode ? "dark" : "light"}.svg" alt="">
+            </div>
+            <div class="img-container">
+                <h2>Code Thinker</h2>
+                <img src="images/undraw_code_thinking_${state.darkMode ? "dark" : "light"}.svg" alt="">
+            </div>
+            <div class="img-container">
+                <h2>Create the Matrix</h2>
+                <img src="images/undraw_hacker_mind_${state.darkMode ? "dark" : "light"}.svg" alt="">
+            </div>
+            <div class="img-container">
+                <h2>Proud Works</h2>
+                <img src="images/undraw_feeling_proud_${state.darkMode ? "dark" : "light"}.svg" alt="">
+            </div>
+        `
+        
+        $images.innerHTML = markup;
+    }
+
+    const init = () => {
+        listeners();
+        render();
+    }
+
+    return {
+        init
+    }
+
+})();
+
+App.init();
