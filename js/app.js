e8fc570e936675ac2ed83d2bad2a5bd7ccfb457a
Dave <dave.cadelina@outlook.com>
2020-12-01 22:31:37 +1100

    added sun and moon icon

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 const toggleTheme = document.querySelector('input')
+const currentMode = document.querySelector('.mode')
 const theDOM = document.querySelector('html')
+const currentIcon = document.querySelector('.fas')
 
 toggleTheme.addEventListener('change', event => {
     if(event.target.checked) {
+        currentMode.innerHTML = "Dark Mode";
         theDOM.setAttribute("data-theme", "dark")
+        currentIcon.classList.replace('fa-sun', 'fa-moon')
+        
     } else {
+        currentMode.innerHTML = "Light Mode";
         theDOM.removeAttribute("data-theme")
+        currentIcon.classList.replace('fa-moon', 'fa-sun')
+        
     }
     console.log("HELLO")
 })
